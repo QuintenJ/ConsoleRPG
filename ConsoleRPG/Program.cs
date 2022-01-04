be41cc7461f492d7614589f623d58982addb23a0
Quinten <me@quintenjames.com>
2022-01-03 23:38:13 -0700

    Test Classes add Story Class

--- ConsoleRPG/Program.cs
+++ ConsoleRPG/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ConsoleRPG;
+
+Story.ScenesSetup();
+Story.StartScene();
+var enemy = new Enemy();
+Console.Write("Enter Character Name: ");
+string playerName = Console.ReadLine();
+
+Console.WriteLine("Thank you......\nBuilding Character......");
+var player = new Character(playerName);
+enemy.DisplayCharacterInfo();
+player.DisplayCharacterInfo();
+player.Heal(player);
+enemy.Heal(enemy);
+player.DisplayCharacterInfo();
+enemy.DisplayCharacterInfo();
+
+enemy.Attack(player);
+player.Attack(enemy);
+player.DisplayCharacterInfo();
+enemy.DisplayCharacterInfo();
+
+
+Story.ReactionScene();
